133b6e2a1e62b92e45a9d7bb3224616ec002b5a8
cjmash <collomash4@gmail.com>
2017-07-12 21:38:25 +0300

    function for testing user submits an empty password while registering

--- bucketlist/test_user.py
+++ bucketlist/test_user.py
@@ -15,11 +15,16 @@ class Usertest(unittest.TestCase):
         self.newUser.users = {}
         result = self.newUser.register( 'email@mail.com', 'name' ,'cj', 'cj')
         self.assertEqual(1,result, "User succesfully created")   
-        
+
      # defining method to test for creating user account with an empty name
     def test_register_null_name(self):
         output=self.newUser.register('test@email.com','','pass','pass')
         self.assertEqual(7,output, "Please fill your name")
+
+        # defining method to test for creating user account with an empty email
+    def test_register_null_email(self):
+        output=self.newUser.register('','name','pass','pass')
+        self.assertEqual(6,output, "Email is Empty ")     
             
 if __name__ == "__main__":
     unittest.main()        
