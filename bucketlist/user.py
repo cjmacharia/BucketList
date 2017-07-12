c610804fd7b2813d4d2d10495b46f1d5a6123cf4
cjmash <collomash4@gmail.com>
2017-07-12 23:04:27 +0300

    edits the login and register functions

--- bucketlist/user.py
+++ bucketlist/user.py
@@ -1,77 +1,72 @@
 from flask import Flask, session, render_template, request, redirect, g, url_for
 import re
-
-users = {}
-
+users = {u'anto@gmail.com': {'pass': u'a', 'name': u'w', 'email': u'anto@gmail.com'}}
 class User():
-	"""
-    Class User allow users to create user accounts, delete user accounts, validate sign up, validate
-    login process and to render feedback messages inform of a dictionary
-    """
 
-    
 
-    # Initializing  class instance variables
-    def __init__(self, name = None, email = None, password = None):
+ 	# Initializing  class instance variables
+    def __init__(self, name=None, email=None, password=None):
         self.name = name
         self.email = email
         self.password = password
 
         #defining method to create account
-    def register(self,email, name,password, cpassword):          
-        if  name!='':
-        	if email!='':
-        		if password!='':
-		            if email not in users.keys():
-		                if password == cpassword:
-		                    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-		                    result = email
-		                    if re.search(regex, result):
-		                        match = re.search(regex, result)
-		                        users[email] = {
-		                        'name': name,
-		                        'email': email,
-		                        'pass': password,
-		                        }
-		                        print(users)
-		                        return  1
-		                    else:
-		                        return 2 
-		                else:
-		                    return 3
-		            else:
-		                return 4
-		        else:
-		    
-		 	        return 5
- 	        else:
- 	        	return 6
-    	else:
-	    	return 7  
+    def register(self,email, name,password, cpassword):       
+            
+        if  name!='' and email!='' and password!='':
 
-	# defining method to validate user
-	def login(self, email, password):
-        if email!='':
-        	if password!='':
-	            if email in users.keys():
-	                result=users[email]
-	                pword=result['pass']
-	                if pword==password:
-	                    return 1
-	                    print ('done')
-	                else:
-	                    return 2
-	            else:
-	                return 3
-	        else:
-	            return 4
-    	else:
-	    	return 5        
+            if email not in users.keys():
 
-    	#function to get a user
-	def get_user_name(self, email):
+                if password==cpassword:
+                    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+                    result = email
+                    if re.search(regex, result):
+                        match = re.search(regex, result)
+                        users[email] = {
+                        'name': name,
+                        'email': email,
+                        'pass': password,
+                        }
+                        print(users)
+                    
+                        return  1
+                    else:
+                        return 2 
+                else:
+                    return 3
+            else:
+                return 4
+        else:
+            return 5
+
+        # defining method to validate user             
+    def login(self, email, password):
+        if email!='' and password!='':
+            if email in users.keys():
+                result=users[email]
+                pword=result['pass']
+                if pword==password:
+                    return 1
+                    print ('done')
+                else:
+                    return 2
+            else:
+                return 3
+        else:
+            return 4  
+
+        #function to get a user's name            
+    def get_user_name(self, email):
         if email in users.keys():
             result =users[email]
             return result['name']
+        else:
+            return False
+
+        #function to get a user's email
+    def get_user_email(self, email):
+        if email in users.keys():
+            result=users[email]
+            return result['email']
         else:
             return False
