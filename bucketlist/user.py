61ba0d17f9debe60f0e8c495b3484dc18f59847c
cjmash <collomash4@gmail.com>
2017-07-12 22:15:00 +0300

    Creates a create user function

--- bucketlist/user.py
+++ bucketlist/user.py
@@ -16,3 +16,35 @@ class User():
         self.name = name
         self.email = email
         self.password = password
+
+        #defining method to create account
+    def register(self,email, name,password, cpassword):          
+        if  name!='':
+        	if email!='':
+        		if password!='':
+		            if email not in users.keys():
+		                if password == cpassword:
+		                    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+		                    result = email
+		                    if re.search(regex, result):
+		                        match = re.search(regex, result)
+		                        users[email] = {
+		                        'name': name,
+		                        'email': email,
+		                        'pass': password,
+		                        }
+		                        print(users)
+		                        return  1
+		                    else:
+		                        return 2 
+		                else:
+		                    return 3
+		            else:
+		                return 4
+		        else:
+		    
+		 	        return 5
+ 	        else:
+ 	        	return 6
+    	else:
+	    	return 7    
