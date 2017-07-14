0d17ac59c98fb08f1aea4628ac08402269f15b23
cjmash <collomash4@gmail.com>
2017-07-14 09:45:39 +0300

    adds a user.py file

---
+++ user.py
@@ -0,0 +1,72 @@
+from flask import Flask, session, render_template, request, redirect, g, url_for
+import re
+users = {u'anto@gmail.com': {'pass': u'a', 'name': u'w', 'email': u'anto@gmail.com'}}
+class User():
+
+
+ 	# Initializing  class instance variables
+    def __init__(self, name=None, email=None, password=None):
+        self.name = name
+        self.email = email
+        self.password = password
+
+        #defining method to create account
+    def register(self,email, name,password, cpassword):       
+            
+        if  name!='' and email!='' and password!='':
+
+            if email not in users.keys():
+
+                if password==cpassword:
+                    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+                    result = email
+                    if re.search(regex, result):
+                        match = re.search(regex, result)
+                        users[email] = {
+                        'name': name,
+                        'email': email,
+                        'pass': password,
+                        }
+                        print(users)
+                    
+                        return  1
+                    else:
+                        return 2 
+                else:
+                    return 3
+            else:
+                return 4
+        else:
+            return 5
+
+        # defining method to validate user             
+    def login(self, email, password):
+        if email!='' and password!='':
+            if email in users.keys():
+                result=users[email]
+                pword=result['pass']
+                if pword==password:
+                    return 1
+                    print ('done')
+                else:
+                    return 2
+            else:
+                return 3
+        else:
+            return 4  
+
+        #function to get a user's name            
+    def get_user_name(self, email):
+        if email in users.keys():
+            result =users[email]
+            return result['name']
+        else:
+            return False
+
+        #function to get a user's email
+    def get_user_email(self, email):
+        if email in users.keys():
+            result=users[email]
+            return result['email']
+        else:
+            return False
